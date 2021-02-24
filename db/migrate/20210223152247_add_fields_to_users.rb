70bc99218d5b86104b66ae9d1cd59543500c6776
Jebitok <sharonkosgei4@gmail.com>
2021-02-24 11:45:10 +0300

    add changes to views & migration

--- db/migrate/20210223152247_add_fields_to_users.rb
+++ db/migrate/20210223152247_add_fields_to_users.rb
@@ -1,7 +1,4 @@
 class AddFieldsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string
-    add_column :users, :username, :string
-    add_index :users, :username, unique: true
   end
 end
